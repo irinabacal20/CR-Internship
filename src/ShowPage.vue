2431ed42885eed2d3fe4e9d221f87396a9200724
irinabacal20 <68803199+irinabacal20@users.noreply.github.com>
2020-09-22 22:02:28 +0300

    Create ShowDescriptionCard Component

--- src/ShowPage.vue
+++ src/ShowPage.vue
@@ -1,20 +1,37 @@
 <template>
-  <div>
-    <h1>Show Page</h1>
+  <div id="show-page">
     <ShowPageHeader/>
+    <div id="show-info-container">
+      <ShowDescriptionCard/>
+    </div>
   </div>
 </template>
 
 <script>
-import ShowPageHeader from "./components/ShowPageHeader";
+import ShowPageHeader from "./components/ShowPageHeader.vue";
+import ShowDescriptionCard from "./components/ShowDescriptionCard.vue";
 
 export default {
   name: "ShowPage",
   components: {
-    ShowPageHeader
+    ShowPageHeader,
+    ShowDescriptionCard
   }
 };
 </script>
 
 <style>
+#show-info-container {
+  margin-left: var(--margin-left);
+  margin-right: var(--margin-right);
+  margin-top: 80px;
+  margin-bottom: 80px;
+}
+
+@media screen and (max-width: 900px) {
+  #show-info-container {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
 </style>
---
+++ src/components/ShowDescriptionCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="show-description-card">
+    <div id="show-card-contents">
+      <img :src="`${searchCardImage}`" alt="Browse Card Image">
+      <div id="show-card-description">
+        <p id="show-card-title">{{showTitle}}</p>
+        <p id="show-card-rating">Rating: {{showRating}}</p>
+        <p>{{showDescription}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowDescriptionCard",
+  data() {
+    return {
+      searchCardImage:
+        "https://images-na.ssl-images-amazon.com/images/S/cmx-images-prod/Item/826174/826174._SX1280_QL80_TTD_.jpg",
+      showTitle: "A.I.C.O Incarnation",
+      showRating: "5/5",
+      showDescription:
+        "In 2035, a biological research project to create an Artificial and Intelligent Cellular Organism (A.I.C.O.) went awry, resulting in an incident called the Burst which transformed Kurobe Gorge into a quarantine area infested by a rampant growth of synthetic organisms called Matter. Two years later, high school student Aiko Tachibana finds that she may be the duplicate of a girl trapped within the Matter whose family disappeared in the Burst."
+    };
+  }
+};
+</script>
+
+<style>
+#show-description-card {
+  background: var(--first-color);
+  box-shadow: var(--box-shadow);
+}
+
+#show-card-contents > img {
+  width: 150px;
+  height: 66, 66%;
+  object-fit: cover;
+  object-position: top;
+}
+
+#show-card-contents {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  font-family: var(--general-font);
+  color: var(--third-color);
+  font-size: 15px;
+  text-align: justify;
+}
+
+#show-card-description {
+  margin-left: 20px;
+}
+
+#show-card-description > p {
+  margin: 10px 20px;
+}
+
+#show-card-title {
+  font-weight: bold;
+  font-size: 18px;
+}
+
+</style>
