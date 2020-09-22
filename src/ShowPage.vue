0921b1b00a51c60cb2bfe11cea746f4c6da2d863
irinabacal20 <68803199+irinabacal20@users.noreply.github.com>
2020-09-22 20:51:04 +0300

    Create ShowPageHeader Displaying the trailer of the Show

--- src/ShowPage.vue
+++ src/ShowPage.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <h1>Show Page</h1>
+    <ShowPageHeader/>
   </div>
 </template>
 
 <script>
+import ShowPageHeader from "./components/ShowPageHeader";
+
 export default {
   name: "ShowPage",
-  components: {}
+  components: {
+    ShowPageHeader
+  }
 };
 </script>
 
---
+++ src/components/ShowPageHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="show-page-header">
+    <iframe
+      id="trailer-header"
+      src="https://www.youtube.com/embed/CCfYQnOLHV0"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShowPageHeader"
+};
+</script>
+
+<style>
+#show-page-header {
+  position: relative;
+  height: 450px;
+  background: var(--second-color);
+}
+
+#trailer-header {
+  width: 100%;
+  height: 450px;
+}
+</style>
